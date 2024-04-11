6d9a0b3564d6400255dcfed4fb01f27367472452
shiennaoscianas <shine.oscianas@gmail.com>
2024-04-11 16:44:31 -0400

    This is a script for Assignment 3: Automated Configuration Assignment - lab3.sh

--- lab3.sh
+++ lab3.sh
@@ -2,15 +2,42 @@
 
 # This script runs the configure-host.sh script from the current directory to modify 2 servers and update the local /etc/hosts file
 
-# Transfer configure-host.sh script to server1-mgmt and apply configurations
+# Function to display verbose output
+verbose_output() {
+    if [ "$verbose" == "true" ]; then
+        echo "$1"
+    fi
+}
+
+#--------------------------------------------------
+
+# Function to transfer configure-host.sh script to server1-mgmt and apply configurations
 scp configure-host.sh remoteadmin@server1-mgmt:/root
-ssh remoteadmin@server1-mgmt -- /root/configure-host.sh -name loghost -ip 192.168.16.3 -hostentry webhost 192.168.16.4 -verbose
+ssh remoteadmin@server1-mgmt -- "/root/configure-host.sh -name loghost -ip 192.168.16.3 -hostentry webhost 192.168.16.4 $verbose"
 
-# Transfer configure-host.sh script to server2-mgmt and apply configurations
+# Function to check if ssh command to server1-mgmt was successful
+if [ $? -eq 0 ]; then
+    verbose_output "Configurations applied successfully on server1-mgmt"
+else
+    echo "Error: Failed to apply configurations on server1-mgmt"
+fi
+
+#--------------------------------------------------
+
+# Function to transfer configure-host.sh script to server2-mgmt and apply configurations
 scp configure-host.sh remoteadmin@server2-mgmt:/root
-ssh remoteadmin@server2-mgmt -- /root/configure-host.sh -name webhost -ip 192.168.16.4 -hostentry loghost 192.168.16.3 -verbose
+ssh remoteadmin@server2-mgmt -- "/root/configure-host.sh -name webhost -ip 192.168.16.4 -hostentry loghost 192.168.16.3 $verbose"
 
-# Update local /etc/hosts file
-./configure-host.sh -hostentry loghost 192.168.16.3
-./configure-host.sh -hostentry webhost 192.168.16.4
+# Function to check if ssh command to server2-mgmt was successful
+if [ $? -eq 0 ]; then
+    verbose_output "Configurations applied successfully on server2-mgmt"
+else
+    echo "Error: Failed to apply configurations on server2-mgmt"
+fi
+
+#--------------------------------------------------
+
+# Function to update local /etc/hosts file
+./configure-host.sh -hostentry loghost 192.168.16.3 $verbose
+./configure-host.sh -hostentry webhost 192.168.16.4 $verbose
 
